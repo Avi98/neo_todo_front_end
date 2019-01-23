bce8c8893baf508de11cf1c6730fdffc98f15b94
webwerks1 <webwerks1@XYZ-123.local>
2019-01-23 16:06:58 +0530

    hot module hooked

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider } from "styled-components";
+
+import configStore from "./reducer/configStore";
+
+const store = configStore();
 
 const size = {
   mobileS: "320px",
@@ -24,13 +29,49 @@ const device = {
   desktop: `(max-width: ${size.desktop})`,
   desktopL: `(max-width: ${size.desktop})`
 };
-ReactDOM.render(
-  <ThemeProvider theme={device}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
-);
+const rootEl = document.getElementById("root");
 
+let render = () => {
+  const App = require("./App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <ThemeProvider theme={device}>
+        <App />
+      </ThemeProvider>
+    </Provider>,
+    rootEl
+  );
+  // Dynamically import our main App component, and render it
+};
+render();
+
+if (module.hot) {
+  // Support hot reloading of components
+  // and display an overlay for runtime errors
+  const renderApp = render;
+  const renderError = error => {
+    const RedBox = require("redbox-react").default;
+    ReactDOM.render(<RedBox error={error} />, rootEl);
+  };
+
+  // In development, we wrap the rendering function to catch errors,
+  // and if something breaks, log the error and render it to the screen
+  render = () => {
+    try {
+      renderApp();
+    } catch (error) {
+      console.error(error);
+      renderError(error);
+    }
+  };
+
+  // Whenever the App component file or one of its dependencies
+  // is changed, re-import the updated component and re-render it
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
