9b7303f9585b32c08a30dcd400c35032946ec92f
webwerks1 <webwerks1@XYZ-123.local>
2019-02-05 22:03:14 +0530

    update todo actions added

--- src/api/todos.js
+++ src/api/todos.js
@@ -13,3 +13,21 @@ export const getTodos = () => (dispatch, getStore) => {
       dispatch({ type: "showError", err });
     });
 };
+export const updateTodoAction = (id, todo_id, payload) => (
+  dispatch,
+  getStore
+) => {
+  const [key] = Object.keys(payload);
+  console.log("`updateTodo_${key}`", `updateTodo_${key}`);
+  fetchData
+    .put(`/todo/${todo_id}`, payload)
+    .then(res => {
+      dispatch({ type: `updateTodo_${key}`, payload, id });
+      dispatch({ type: "stopLoading" });
+    })
+    .catch(err => {
+      dispatch({ type: "stopLoading" });
+      dispatch({ type: "showError", err });
+    });
+  dispatch({ type: "startLoading" });
+};
