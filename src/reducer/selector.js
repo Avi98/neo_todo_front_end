2da1c1814deb422d733fc3982621026052959300
webwerks1 <webwerks1@XYZ-123.local>
2019-02-05 15:56:39 +0530

    NEEDS SOME REFATORING

--- src/reducer/selector.js
+++ src/reducer/selector.js
@@ -6,16 +6,24 @@ export const todoSelector = createSelector(
   orm,
   state => state.orm,
   session => {
-    console.log(
-      "session.TodoModel.all().toModelArray()",
-      session.TodoModel.all().toModelArray()
-    );
-    console.log(
-      "session.DateModel.all().toModelArray()",
-      session.DateModel.all().toModelArray()
-    );
-    //   const datekey = session.TodoModel.all()
-    //     .toModelArray()
-    //     .map();
+    // console.log(
+    //   "session.TodoModel.all().toModelArray()",
+    //   session.TodoModel.all().toModelArray()
+    // );
+    // console.log(
+    //   "session.DateModel.all().toModelArray()",
+    //   session.DateModel.all().toModelArray()
+    // );
+    const todoData = session.TodoModel.all()
+      .toModelArray()
+      .map((item, i) => {
+        const todoObj = {
+          ...item.ref
+        };
+
+        return todoObj;
+      });
+    return todoData;
+    // rename selectors, create a object of obj with date a key of todos like: { "15 jan 19": {id: "...". complete: true } }
   }
 );
